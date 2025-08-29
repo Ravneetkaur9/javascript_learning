a93d3429c70cd80a8d859eaf092584898c1be7de
Ravneetkaur9 <ravneetkaur13@rrc.ca>
2025-08-29 13:43:17 -0500

    learning about variables

---
+++ chapter1/sandbox.js
@@ -0,0 +1,13 @@
+let ninjas = ['rav','san','kp'];
+
+//ninjas[1] = 'ken';
+//console.log(ninjas[1]);
+
+// let result = ninjas.join('-');
+
+
+// let result = ninjas.concat(['ken','sam']);
+
+let result = ninjas.push('ken');
+
+console.log(result);
