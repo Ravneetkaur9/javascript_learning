044357bb0cacb6a483f5cc115b4f844aca5683b1
Ravneetkaur9 <ravneetkaur13@rrc.ca>
2025-09-01 14:23:53 -0500

    control flow practice

--- Practice/sandbox.js
+++ Practice/sandbox.js
@@ -11,3 +11,23 @@ console.log(a, b);
 let num = 13;
 console.log(num % 2 === 0 ? "Even" : "Odd");
 
+// control flow 
+
+let marks = 85;
+if (marks >= 90) console.log("A");
+else if (marks >= 80) console.log("B");
+else if (marks >= 70) console.log("C");
+else console.log("Fail");
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`7 x ${i} = ${7 * i}`);
+}
+
+let day = 3;
+switch(day) {
+  case 1: console.log("Monday"); break;
+  case 2: console.log("Tuesday"); break;
+  case 3: console.log("Wednesday"); break;
+  // ... add others
+  default: console.log("Invalid");
+}
