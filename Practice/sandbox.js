733ca2b6f0a6591c8b630b0df74b52c602584835
Ravneetkaur9 <ravneetkaur13@rrc.ca>
2025-09-01 14:23:09 -0500

    trying something different

---
+++ Practice/sandbox.js
@@ -0,0 +1,13 @@
+// extra practice
+
+let name = "Ravneet";
+let age = 22;
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+let a = 5, b = 10;
+[a, b] = [b, a];  // swap using array destructuring
+console.log(a, b);
+
+let num = 13;
+console.log(num % 2 === 0 ? "Even" : "Odd");
+
